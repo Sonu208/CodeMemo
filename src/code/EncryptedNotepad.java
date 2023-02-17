1dda1ca76f84689fd4181f2c96978a58457ecd6e
Sonu Pradhan <sonupradhan208@gmail.com>
2023-02-17 21:49:49 +0530

    first commit

---
+++ src/code/EncryptedNotepad.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author SONU
+ */
+public class EncryptedNotepad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
